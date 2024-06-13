b91be5bc20635adc404b5cdc77875006de303b48
Shahid Qasim <161705646+ShahidQasim@users.noreply.github.com>
2024-06-13 23:50:45 +0500

    Add files via upload

---
+++ Assignment(Q#05)/main.js
@@ -0,0 +1,4 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = " unknown ";
+console.log("the famous person" + famous_person + "once said: do whatever u want");
+console.log("the famous person ".concat(famous_person, " once said: do whatever u want"));
