69b0af4b5e181e38a675c7a4feecfcbb6369c4f8
Shahid Qasim <161705646+ShahidQasim@users.noreply.github.com>
2024-06-13 22:41:48 +0500

    Delete Assignment(Q#03)/Assignment (Q#02) directory

--- Assignment(Q#03)/Assignment (Q#02)/main.ts
+++
@@ -1,6 +0,0 @@
-// name cases: strore person name in a variable then print this name in upper,lower case and title case
-let personname: string = "Shahid"
-let print: string= (personname.toUpperCase());
-let print2: string=(personname.toLowerCase())
-console.log(print2)
-console.log(print)
