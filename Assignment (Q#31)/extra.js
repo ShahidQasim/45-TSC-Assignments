a523327ce5fa1e68bf2330b5b51daece7aea2342
Shahid Qasim <161705646+ShahidQasim@users.noreply.github.com>
2024-06-13 17:38:50 +0500

    Delete Assignment (Q#31) directory

--- Assignment (Q#31)/extra.js
+++
@@ -1,9 +0,0 @@
-var a = 1;
-var b = 3;
-a++;
-b--;
-console.log("a", a++); // result =2
-console.log("a", a); //after post increment  //result =3
-console.log("b", b); // result =2
-console.log("b", b--); // result =2
-console.log("b", b); // result =1
